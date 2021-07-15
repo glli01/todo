84ff60b40064c166da4b09b4ab81ec2a541b1f29
GiantTeddyBear <giantteddybear1@gmail.com>
2021-07-15 15:02:02 -0700

    Add check marks into code

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-
+import checkmarkFilled from "../assets/img/checkmark-filled.svg";
+import checkmarkUnfilled from "../assets/img/checkmark-unfilled.svg";
 const Task = ({ item }) => {
   return (
     <div className={item.isCompleted ? "task completed" : "task"}>
-      {item.isCompleted ? <span> </span> : <span> </span>} {item.title}
+      {item.isCompleted ? (
+        <img src={checkmarkFilled}></img>
+      ) : (
+        <img src={checkmarkUnfilled}></img>
+      )}{" "}
+      {item.title}
       {/* {item.description} */}
     </div>
   );
