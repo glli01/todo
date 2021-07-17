01ea6a3211a1a90cf97b3f63cf93401e5f89c49d
GiantTeddyBear <giantteddybear1@gmail.com>
2021-07-16 20:31:40 -0700

    use placeholder text

--- frontend/src/components/addTask.jsx
+++ frontend/src/components/addTask.jsx
@@ -3,10 +3,11 @@ import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 const AddTask = () => {
   const dispatch = useDispatch();
-  const [taskText, setTaskText] = useState("add a task here");
+  const [taskText, setTaskText] = useState("");
   const handleKeyDown = (e) => {
     const trimmedText = taskText.trim();
     if (e.which === 13 && trimmedText) {
+      console.log("got enter key");
     }
   };
   return (
@@ -15,6 +16,7 @@ const AddTask = () => {
       value={taskText}
       onChange={(e) => setTaskText(e.currentTarget.value)}
       onKeyDown={handleKeyDown}
+      placeholder="Add a task..."
       className="task__input"
     ></input>
   );
