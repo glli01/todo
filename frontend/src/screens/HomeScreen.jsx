8c11e45c376e316251e3342e909b8ec7cd2784a3
GiantTeddyBear <giantteddybear1@gmail.com>
2021-07-31 03:25:21 -0700

    uibuild

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,19 +1,9 @@
 import React from "react";
-import ConfirmOverlay from "./ConfirmOverlay";
-import { useState } from "react";
 const HomeScreen = () => {
-  const [state, setState] = useState("hello");
   return (
     <>
       <div>
-        <div className="home__title"> Today </div>
-        <div className="home__text--center">
-          The forecast is cloudy with a chance of meatballs.
-          <br />
-          W.I.P.
-          <br />
-          {state}
-        </div>
+        <div className="home__title"> Welcome to todooos </div>
       </div>
     </>
   );
