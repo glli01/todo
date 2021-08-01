892391b9cf791c3ce89492acf6f627b30ecbd395
GiantTeddyBear <giantteddybear1@gmail.com>
2021-08-01 12:23:45 -0700

    minor ui fixes

--- frontend/src/components/SidebarLink.jsx
+++ frontend/src/components/SidebarLink.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Icon from "./Icon";
+import { useDispatch } from "react-redux";
+import { LIST_SET_ACTIVE } from "../features/lists/constants/listsConstants.js";
 const SidebarLink = ({ pathName, path, icon, iconActive, title }) => {
+  const dispatch = useDispatch();
   return (
     <Link
       to={path}
       className={pathName.toLowerCase() !== path.toLowerCase() ? "" : "active"}
+      onClick={() => dispatch({ type: LIST_SET_ACTIVE, payload: null })}
     >
       <div className="sidebar__category-title">
         <span className="wrapper--fs">
